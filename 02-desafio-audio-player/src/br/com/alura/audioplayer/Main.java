2b8502ae111a835e54434ea75b9b9e8fe81fbc54
Fernanda Amaral <camaralfernanda@gmail.com>
2026-05-24 13:02:58 -0300

    cria classe Musica e finaliza desafio com simulacao de ambos os audios

--- 02-desafio-audio-player/src/br/com/alura/audioplayer/Main.java
+++ 02-desafio-audio-player/src/br/com/alura/audioplayer/Main.java
@@ -1,15 +1,18 @@
 package br.com.alura.audioplayer;
 
 import br.com.alura.audioplayer.modelos.Podcast;
+import br.com.alura.audioplayer.modelos.Musica;
 
 public class Main {
     public static void main(String[] args) {
+
+        // --- INSTANCIA PODCAST ---
         Podcast meuPodcast = new Podcast();
         meuPodcast.setTitulo("Casefile True Crime");
         meuPodcast.setHost("Casey");
         meuPodcast.setDescricao("Um podcast sobre crimes reais que se aprofunda nas histórias \nde casos solucionados, e também arquivados, ao redor do mundo.");
 
-        //Simulando interações
+        // Simulando interações
         for (int i = 0; i < 500; i++) {
             meuPodcast.reproduzir();
         }
@@ -18,7 +21,7 @@ public class Main {
             meuPodcast.curtir();
         }
 
-        //Exibindo dados
+        // Exibindo dados
         System.out.println("--- DETALHES DO PODCAST ---");
         System.out.println("Título: " + meuPodcast.getTitulo());
         System.out.println("Apresentado por: " + meuPodcast.getHost());
@@ -26,6 +29,33 @@ public class Main {
         System.out.println("Total de Reproduções: " + meuPodcast.getTotalReproducoes());
         System.out.println("Total de Curtidas: " + meuPodcast.getTotalCurtidas());
         System.out.println("Classificação: " + meuPodcast.getClassificacao() + " Estrelas!");
+
+        System.out.println("\n----------------------------------------------\n");
+
+        // --- INSTANCIA MUSICA ---
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Dust In The Wind");
+        minhaMusica.setArtista("Kansas");
+        minhaMusica.setAlbum("Point of Know Return");
+        minhaMusica.setGenero("Soft rock");
+
+        // Simulando Interações
+        for (int i = 0; i < 250; i++) {
+            minhaMusica.reproduzir();
+        }
+
+        for (int i = 0; i < 150; i ++) {
+            minhaMusica.curtir();
+        }
+
+        System.out.println("--- DETALHES DA MÚSICA ---");
+        System.out.println("Título: " + minhaMusica.getTitulo());
+        System.out.println("Artista: " + minhaMusica.getArtista());
+        System.out.println("Álbum: " + minhaMusica.getAlbum());
+        System.out.println("Total de Reproduções: " + minhaMusica.getTotalReproducoes());
+        System.out.println("Total de Curtidas: " + minhaMusica.getTotalCurtidas());
+        System.out.println("Classificação (Estrelas): " + minhaMusica.getClassificacao());
+
     }
 
 }
