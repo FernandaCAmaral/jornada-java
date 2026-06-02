7e15aacec0c86051454eb3ec20f79b235aaabbe3
Fernanda Amaral <camaralfernanda@gmail.com>
2026-06-02 06:44:05 -0300

    feat(modelos): implementa classe CartaoCredito com regras de validacao de saldo

--- 03-desafio-aplicacao-de-compras/src/br/com/alura/appcompras/modelos/CartaoCredito.java
+++ 03-desafio-aplicacao-de-compras/src/br/com/alura/appcompras/modelos/CartaoCredito.java
@@ -1,6 +1,39 @@
 package br.com.alura.appcompras.modelos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartaoCredito {
     private double limite;
     private double saldo;
+    private List<Compra> compras;
+
+    public CartaoCredito(double limite) {
+        this.limite = limite;
+        //quando o cartão é criado o saldo inicial é igual ao limite
+        this.saldo = limite;
+        this.compras = new ArrayList<>(); // inicializa a lista vazia
+    }
+
+    // Regra de Negócio
+    public boolean lancaCompra(Compra compra) {
+        if (this.saldo >= compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
 }
